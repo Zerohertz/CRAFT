d1b54ee6d9013f15e9d27dcc96bf3412a4085966
Zerohertz <ohg3417@gmail.com>
2023-03-10 10:55:20 +0900

    :tada: init: utils

---
+++ utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/src/convex_hull.cpp" "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/src/convex_hull.cpp.o"
+  "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/src/dilate.cpp" "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/src/dilate.cpp.o"
+  "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/src/find_components.cpp" "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/src/find_components.cpp.o"
+  "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/src/rotated_bounding_box.cpp" "/home/jovyan/local/1_user/hgoh@agilesoda.ai/TwinReader/CRAFT/utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/src/rotated_bounding_box.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ utils/cpp_bindings/build/CMakeFiles/fast_boxes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fast_boxes.dir/src/convex_hull.cpp.o"
+  "CMakeFiles/fast_boxes.dir/src/dilate.cpp.o"
+  "CMakeFiles/fast_boxes.dir/src/find_components.cpp.o"
+  "CMakeFiles/fast_boxes.dir/src/rotated_bounding_box.cpp.o"
+  "libfast_boxes.pdb"
+  "libfast_boxes.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fast_boxes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
