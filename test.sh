e7b8d344f74f66c052a22b880a04562491753ade
Zerohertz <ohg3417@gmail.com>
2023-03-10 12:51:20 +0900

    :bug: Update: test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 read -p "Exp: " dir
 rm -rf ./exp/$dir
+cd utils/cpp_bindings
+sh compile.sh
+cd ../..
+
 CUDA_VISIBLE_DEVICES=1 python test.py --yaml=org --dir=$dir
 # CUDA_VISIBLE_DEVICES=1 python test.py --yaml=main --dir=$dir
 cp ./config/main.yaml ./exp/$dir/
